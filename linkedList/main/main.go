1b9aa2034c56dc4b466a83466d40c93fa2386894
iceymoss <ice_moss@163.com>
2023-08-22 16:41:27 +0800

    fix

--- linkedList/main/main.go
+++ linkedList/main/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "codeCaprice/linkedList"
+import (
+	"codeCaprice/linkedList"
+	"fmt"
+)
 
 func main() {
 	link := &linkedList.ListNode{7, &linkedList.ListNode{7, &linkedList.ListNode{7, &linkedList.ListNode{7, nil}}}}
 	linkedList.RemoveElements(link, 7)
+	fmt.Println("git")
+	linkedList.Constructor()
 }
