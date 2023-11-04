85fcce9e288346096d4cf42661e5cfea494e5f6b
iceymoss <ice_moss@163.com>
2023-11-04 23:31:51 +0800

    feat:meituan1104

---
+++ meituan1104/q3/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, q int
+	fmt.Scan(&n, &q)
+
+	var str string
+	fmt.Scan(&str)
+
+	inds := make([]int, q)
+	for i := 0; i < q; i++ {
+		var index int
+		fmt.Scan(&index)
+		inds[i] = index
+	}
+	fmt.Println(n)
+	fmt.Println(q)
+	fmt.Println(str)
+	fmt.Println(inds)
+	fmt.Println()
+
+	t := str
+	bt := []byte(str)
+
+	for len(bt) > 0 {
+		if len(bt) != 0 {
+			bt = bt[:len(bt)-1]
+			t += string(bt)
+		}
+		if len(bt) != 0 {
+			bt = bt[1:]
+			t += string(bt)
+		}
+	}
+	fmt.Println("结果：", t)
+	for _, v := range inds {
+		fmt.Println(string(t[v-1]))
+	}
+}
