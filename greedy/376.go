f970ad5271cf06e4a44f7d3b5a47a7455af95cdf
iceymoss <ice_moss@163.com>
2023-11-20 21:30:33 +0800

    feat:贪心算法

--- greedy/376.go
+++ greedy/376.go
@@ -5,7 +5,7 @@ func wiggleMaxLength(nums []int) int {
 	if len(nums) < 2 {
 		return n
 	}
-	ans := 1
+	ans := 1 //默认只有两个元素，如果相同ans = 1, 不相同ans = 2，解释：仅有一个元素或者含两个不等元素的序列也视作摆动序列。
 	perDiff := nums[1] - nums[0]
 	if perDiff != 0 {
 		ans = 2
